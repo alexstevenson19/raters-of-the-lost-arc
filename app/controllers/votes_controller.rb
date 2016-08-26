20e2c9ec79d7d36e6fcbd14bf4d5eb3c558c19ad
Alex S & Matthew Moran <pair+alexstevenson19.mjmoran123@devbootcamp.com>
2016-08-26 10:44:17 -0500

    voting in reviews controller, and reviews show page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,15 @@ class VotesController < ApplicationController
   def create
     @review = Review.find(params[:id])
     author = @review.reviewer
+    @film = @review.film
     if current_user && current_user.id != author.id && !current_user.voted?(@review.id)
       Vote.create(voter_id: current_user.id, review_id: @review.id)
-      redirect_to review_path(id: @review.id, film_id: @review.film.id)
+      render "reviews#show"
+      # redirect_to review_path(id: @review.id, film_id: @review.film.id)
     else
       @errors = ["You can't vote!"]
-      redirect_to review_path(id: @review.id, film_id: @review.film.id)
+      render "reviews#show"
+      redirect_to review_path(id: @review.id, film_id: @review.film.id, )
     end
   end
 end
