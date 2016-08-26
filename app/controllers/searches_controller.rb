fe1fbe7f2943dcdb78c199b310b177e2b745f9e3
mmazanec22 <melanie.mazanec@gmail.com>
2016-08-26 13:58:06 -0500

    Search feature functions if you don't click a non-functioning link after

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,26 @@ class SearchesController < ApplicationController
   protect_from_forgery except: :show
 
   def show
-    # if param[:search_type] == "Film"
-      #search film title, description, release_year
-    # elsif param[:search_type] == "Review"
-      #search review headline, content, reviewer
-    # end
+    search_query = params[:search_string]
+    search_array = search_query.split(" ")
+    search_array.concat(search_array.map{|str| str.downcase})
+    search_array.concat(search_array.map{|str| str.upcase})
+    search_array.concat(search_array.map{|str| str.capitalize})
+    @search_return = []
+    if params[:search_type] == "Films"
+      search_array.each do |search_string|
+        @search_return.concat(Film.all.find_all {|film| film.title.include?(search_string)})
+        @search_return.concat(Film.all.find_all {|film| film.description.include?(search_string)})
+        @search_return.concat(Film.all.find_all {|film| film.release_year.to_s.include?(search_string)})
+      end
+    else
+      search_array.each do |search_string|
+        @search_return.concat(Review.all.find_all {|review| review.headline.include?(search_string)})
+        @search_return.concat(Review.all.find_all {|review| review.content.include?(search_string)})
+        @search_return.concat(Review.all.find_all {|review| review.reviewer.username.include?(search_string)})
+      end
+    end
+    @search_return
   end
 
 end
