3a70eeae08a018944decc4dc0881113ea1246250
Alex S & Matthew Moran <pair+alexstevenson19.mjmoran123@devbootcamp.com>
2016-08-26 15:15:56 -0500

    add real movies to seed file, and protect profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?, :ensure_login!, :top_movies, :recent_reviews
 
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
